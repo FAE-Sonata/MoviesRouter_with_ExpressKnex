1b38aea9d6e0154d043d5db25d6cc778edb4cad1
FAE-Sonata <HeYu1993@gmail.com>
2021-07-19 16:11:44 -0400

    resetting knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,20 +1,16 @@
-// const path = require("path");
-
-// if (process.env.USER) require("dotenv").config();
-
-// const {
-//   DATABASE_URL = "postgresql://postgres@localhost/postgres",
-// } = process.env;
 const path = require("path");
-require("dotenv").config();
-const { DATABASE_URL } = process.env;
+
+if (process.env.USER) require("dotenv").config();
+
+const {
+  DATABASE_URL = "postgresql://postgres@localhost/postgres",
+} = process.env;
 
 module.exports = {
   development: {
-    client: 'postgresql',
-    connection: DATABASE_URL, /* {
-      filename: './dev.sqlite3'
-    } */
+    client: "postgresql",
+    connection: DATABASE_URL,
+    pool: { min: 0, max: 5 },
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
     },
@@ -22,41 +18,30 @@ module.exports = {
       directory: path.join(__dirname, "src", "db", "seeds"),
     },
   },
-  // development: {
-  //   client: "postgresql",
-  //   connection: DATABASE_URL,
-  //   pool: { min: 0, max: 5 },
-  //   migrations: {
-  //     directory: path.join(__dirname, "src", "db", "migrations"),
-  //   },
-  //   seeds: {
-  //     directory: path.join(__dirname, "src", "db", "seeds"),
-  //   },
-  // },
 
-  // production: {
-  //   client: "postgresql",
-  //   connection: DATABASE_URL,
-  //   pool: { min: 0, max: 5 },
-  //   migrations: {
-  //     directory: path.join(__dirname, "src", "db", "migrations"),
-  //   },
-  //   seeds: {
-  //     directory: path.join(__dirname, "src", "db", "seeds"),
-  //   },
-  // },
+  production: {
+    client: "postgresql",
+    connection: DATABASE_URL,
+    pool: { min: 0, max: 5 },
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    },
+  },
 
-  // test: {
-  //   client: "sqlite3",
-  //   connection: {
-  //     filename: ":memory:",
-  //   },
-  //   migrations: {
-  //     directory: path.join(__dirname, "src", "db", "migrations"),
-  //   },
-  //   seeds: {
-  //     directory: path.join(__dirname, "src", "db", "seeds"),
-  //   },
-  //   useNullAsDefault: true,
-  // },
+  test: {
+    client: "sqlite3",
+    connection: {
+      filename: ":memory:",
+    },
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    },
+    useNullAsDefault: true,
+  },
 };
